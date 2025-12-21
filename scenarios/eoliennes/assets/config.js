10349cf52b5549280f4b8b6372884e306fbfbcd3
Nix177 <nicolas.tuor@bluewin.ch>
2025-12-21 21:36:21 +0100

    Update config.js

--- scenarios/eoliennes/assets/config.js
+++ scenarios/eoliennes/assets/config.js
@@ -1,6 +1,6 @@
 // assets/config.js
 // On pointe vers la racine du worker (sans /api à la fin)
-export const API_BASE = "https://dark-rice-25ee.nicolas-tuor.workers.dev";
-
+// scenarios/eoliennes/assets/config.js
+export const API_BASE = "https://dark-rice-25ee.nicolas-tuor.workers.dev/api";
 // Active la mémoire R2 si et seulement si /api/memory est déployé
 export const HAS_MEMORY = false;
